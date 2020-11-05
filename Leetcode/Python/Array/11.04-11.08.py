3fd375d0de2dc49be8b2d96874a1971c4326afb6
xiaoxuan liang <41559287+liangxiaoxuan@users.noreply.github.com>
2020-11-04 21:40:51 -0500

    Update 11.04-11.08.py

--- Leetcode/Python/Array/11.04-11.08.py
+++ Leetcode/Python/Array/11.04-11.08.py
@@ -6,5 +6,13 @@ Input: nums = [2,7,11,15], target = 9
 Output: [0,1]
 Output: Because nums[0] + nums[1] == 9, we return [0, 1].
 
+def twoSum(self, nums, target):
+  
+  
+  
+2. Given a 32-bit signed integer, reverse digits of an integer.
+Example:
+Input: x = 123
+Output: 321
 
 
