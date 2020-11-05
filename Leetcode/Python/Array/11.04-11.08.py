f452d89a7f9522ca8a0e72ec72446a43c375d065
xiaoxuan liang <41559287+liangxiaoxuan@users.noreply.github.com>
2020-11-05 12:49:42 -0500

    Update 11.04-11.08.py

--- Leetcode/Python/Array/11.04-11.08.py
+++ Leetcode/Python/Array/11.04-11.08.py
@@ -6,7 +6,28 @@ Input: nums = [2,7,11,15], target = 9
 Output: [0,1]
 Output: Because nums[0] + nums[1] == 9, we return [0, 1].
 
-def twoSum(self, nums, target):
+def twosum(nums, target):
+    """
+    :param nums:
+    :param target:
+    :return:
+    """
+    dic = {}
+    result = []
+    for index, num in enumerate(nums):
+        a = target - num
+        if a not in dic:  # append num into dic
+            dic[num] = index  # if a !=  num
+        else:
+            result.append([dic[a], index])  # output is index
+
+    print(result)
+
+
+if __name__ == '__main__':
+    nums = [1, 2, 7, 8, 9, 11]
+    target = 9
+    twosum(nums, target)
   
   
   
