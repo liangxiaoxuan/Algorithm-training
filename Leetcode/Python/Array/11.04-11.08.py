3d3747cb2155f16c81b7a261f70b6cf56caf7a69
xiaoxuan liang <41559287+liangxiaoxuan@users.noreply.github.com>
2020-11-05 12:50:54 -0500

    Update 11.04-11.08.py

--- Leetcode/Python/Array/11.04-11.08.py
+++ Leetcode/Python/Array/11.04-11.08.py
@@ -1,10 +1,10 @@
 ## 11.04
 
-1.Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
-example:
-Input: nums = [2,7,11,15], target = 9
-Output: [0,1]
-Output: Because nums[0] + nums[1] == 9, we return [0, 1].
+# 1.Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+# example:
+# Input: nums = [2,7,11,15], target = 9
+# Output: [0,1]
+# Output: Because nums[0] + nums[1] == 9, we return [0, 1].
 
 def twosum(nums, target):
     """
@@ -31,9 +31,9 @@ if __name__ == '__main__':
   
   
   
-2. Given a 32-bit signed integer, reverse digits of an integer.
-Example:
-Input: x = 123
-Output: 321
+# 2. Given a 32-bit signed integer, reverse digits of an integer.
+# Example:
+# Input: x = 123
+# Output: 321
 
 
