cfaa65d47adf67eb25671309bf048fc027ec17f0
xiaoxuan liang <41559287+liangxiaoxuan@users.noreply.github.com>
2020-11-04 21:39:21 -0500

    Update and rename 11.04-11.08 to 11.04-11.08.py

---
+++ Leetcode/Python/Array/11.04-11.08.py
@@ -0,0 +1,10 @@
+## 11.04
+
+1.Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+example:
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Output: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+
+
